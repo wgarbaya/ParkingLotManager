d7e24264306d691f3b0c7511742668380159aab2
wgarbaya <walid_g_91@hotmail.com>
2019-09-21 12:04:23 +0200

    adding Client Exp

---
+++ parkingclientapi/src/main/java/fr/walidg/park/parkingclientapi/App.java
@@ -0,0 +1,42 @@
+package fr.walidg.park.parkingclientapi;
+
+import java.time.LocalDateTime;
+
+import fr.walidg.park.parking.ParkingLotBuilder;
+import fr.walidg.park.parking.ParkingLotManager;
+import fr.walidg.park.parking.ParkingClient.Electic20KwVehicule;
+import fr.walidg.park.parking.ParkingStructure.ParkingFullException;
+import fr.walidg.park.parking.ParkingStructure.entity.Slot;
+
+public class App 
+{
+	public App() {
+	}
+	public static Float price(Slot lo) {
+		return 0f;
+	}
+
+	public static void main( String[] args )
+	{
+		ParkingLotBuilder plb = new ParkingLotBuilder();
+		ParkingLotManager plm = plb
+				.withElectric20Slots(1)
+				.withElectric50Slots(0)
+				.withPetrolSlots(10)
+				.withPrincing(ParkingLotBuilder::FixedAndTimePricing)
+				.build();
+		try {
+			Electic20KwVehicule v = new Electic20KwVehicule("ID");
+			Slot s = plm.getASlot(v);
+			s.setOccupiedSince(LocalDateTime.now().minusDays(1));
+			Float price = plm.freeASlot(v);
+			System.out.println(String.format("The price is :  %10.2f €", price));
+
+		} catch (ParkingFullException e) {
+			e.printStackTrace();
+		} catch(IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
